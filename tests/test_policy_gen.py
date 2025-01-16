7c8142913c7da8a51a85ff844a583954cdf7b1cb
krosskinetic <anshumaanvikramsinghlucknow@gmail.com>
2025-01-16 18:02:19 +0530

    added a summary pipeline to synth_policy

--- tests/test_policy_gen.py
+++ tests/test_policy_gen.py
@@ -1,6 +1,6 @@
 import argparse
 import pandas as pd
-from utils.inference import create_policy_chain, format_time_series
+from utils.inference import create_policy_chain, format_time_series, summarize_policy_chain
 from utils.inference import get_anthropic_llm, get_gemini_llm, get_openai_llm
 import logging
 from dotenv import load_dotenv
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     llms = {
         "gemini": get_gemini_llm(0.8),
-        "openai": get_openai_llm(0.8),
-        "anthropic": get_anthropic_llm(0.8),
+        #"openai": get_openai_llm(0.8),
+        #"anthropic": get_anthropic_llm(0.8),
     }
 
     for name, llm in llms.items():
@@ -27,18 +27,22 @@ if __name__ == "__main__":
         print(llm)
         logger.info(llm)
 
+        summary_chain = summarize_policy_chain(llm) # Use the same model or a different one if one model performs better for summarization.
         policy_chain = create_policy_chain(llm)
         results = []
         for i, row in data.iterrows():
             print(i)
+
             time_series = format_time_series(row["history"])
             description = row["title"]
+            policy_summary = summary_chain.invoke({"policy_description": description}).content
 
             logger.info(description)
             logger.info(time_series)
+            logger.info(policy_summary)
 
             res = policy_chain.invoke(
-                {"description": description, "time_series": time_series}
+                {"description": description, "time_series": time_series, "policy_summary": policy_summary}
             )
 
             print(res.content)
--- utils/inference.py
+++ utils/inference.py
@@ -23,6 +23,9 @@ def get_api_key(key_name: str = "ANTHROPIC_API_KEY") -> str:
 
 
 def get_gemini_llm(temp: float):
+
+    '''Creates an instance of the ChatGoogleGenerativeAI class using gemini 1.5 pro LLM with the specified temperature.'''
+
     api_key = get_api_key("GOOGLE_API_KEY")
     llm = ChatGoogleGenerativeAI(
         model="gemini-1.5-pro",
@@ -34,6 +37,9 @@ def get_gemini_llm(temp: float):
 
 
 def get_openai_llm(temp: float):
+
+    '''Creates an instance of the ChatOpenAI class using gpt-4o-mini LLM with the specified temperature.'''
+
     api_key = get_api_key("OPENAI_API_KEY")
     llm = ChatOpenAI(
         model="gpt-4o-mini",
@@ -45,6 +51,9 @@ def get_openai_llm(temp: float):
 
 
 def get_anthropic_llm(temp: float):
+
+    '''Creates an instance of the ChatAnthropic class using claude 3.5 sonnet LLM with the specified temperature.'''
+
     api_key = get_api_key("ANTHROPIC_API_KEY")
     llm = ChatAnthropic(
         model="claude-3-5-sonnet-20241022",
@@ -57,11 +66,14 @@ def get_anthropic_llm(temp: float):
 
 
 def create_policy_chain(llm):
+
+    '''Creates a pipeline that prompts the LLM to identify a policy based on a description of the policy and a time series.'''
+    
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "You are given a time series from real world data and its description. You are responsible for"
+                "You are given a time series from real world data, its description and its summary. You are responsible for"
                 "identifying a policy that may have affected the time series."
                 "You may reason in a block enclosed by <SCRATCH></SCRATCH>. Afterwards, only the description of the"
                 "policy should be returned.",
@@ -70,7 +82,31 @@ def create_policy_chain(llm):
                 "user",
                 "Identify a policy that may have affected this time series."
                 "Description:\n{description}\n"
-                "Time Series Data:\n{time_series}",
+                "Time Series Data:\n{time_series}"
+                "Policy Summary: \n{policy_summary}",
+            ),
+        ]
+    )
+
+    chain = prompt | llm
+
+    return chain
+
+def summarize_policy_chain(llm):
+    
+    '''Creates a pipeline that prompts the LLM to summarize a policy based on a description of the policy.'''
+    
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are given a description of a policy and are responsible for summarizing the policy."
+                "You may reason in a block enclosed by <SCRATCH></SCRATCH>. Afterwards, only the summary of the"
+                "policy should be returned.",
+            ),
+            (
+                "user",
+                "Summarize the policy described below:\n{policy_description}",
             ),
         ]
     )
